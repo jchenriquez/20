26115b95e286f02fe71beaf8d3d1cad34575146a
jchenriquez <jc.henriquez04@gmail.com>
2019-11-30 17:12:08 -0500

    Matching parenthesis solution using stack.

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+var PAREN_MATCH = map[rune]rune {
+	')': '(',
+	']': '[',
+	'}': '{',
+}
+
+func isValid(s string) bool {
+	stack := make([]rune, 0, len(s))
+
+	for _, char := range s {
+		opening, isClosing := PAREN_MATCH[char]
+
+		if isClosing {
+			if len(stack) == 0 {
+				return false
+			}
+
+			if opening != stack[len(stack)-1] {
+				return false
+			}
+
+			stack = stack[:len(stack)-1]
+		} else {
+			stack = append(stack, char)
+		}
+	}
+
+	return true
+}
+
+func main() {
+	fmt.Printf("isValid %v\n", isValid("{[]}"))
+}
